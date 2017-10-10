e8707f9d3fd3988dbda94c23196fc966db2f9b34
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-10-10 10:36:04 -0300

    produto perecivel, pedido, venda

--- Código/Tabacaria/src/model/loja/Venda.java
+++ Código/Tabacaria/src/model/loja/Venda.java
@@ -2,7 +2,7 @@ package model.loja;
 
 import model.notasFiscais.NotaFiscalVenda;
 
-public class Venda extends Pedido {
+public class Venda  {
 
 	private int id;
 
@@ -10,4 +10,35 @@ public class Venda extends Pedido {
 
 	private int valorTotal;
 
+    public Venda(int id, ProdutoLoja[] produtos, int valorTotal) {
+        this.id = id;
+        this.produtos = produtos;
+        this.valorTotal = valorTotal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ProdutoLoja[] getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ProdutoLoja[] produtos) {
+        this.produtos = produtos;
+    }
+
+    public int getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(int valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+        
 }
