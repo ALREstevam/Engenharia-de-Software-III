09535fd2a915f056f83ae10f929765dbda1447f4
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-10-10 09:54:28 -0300

    classe fornecedor

--- Código/Tabacaria/src/model/fornecedores/Produto.java
+++ Código/Tabacaria/src/model/fornecedores/Produto.java
@@ -8,6 +8,47 @@ public class Produto {
 
 	private String descricao;
 
-	private int preco;
+	private float preco;
+
+    public Produto(int id, String nome, String descricao, float preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    
 
 }
