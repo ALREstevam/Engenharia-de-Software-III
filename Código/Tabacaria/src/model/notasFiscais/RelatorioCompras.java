3657fca8a7666e1c982c4b6a865b572d3e6b8468
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-10-14 19:22:47 -0300

    arrumando classe

--- Código/Tabacaria/src/model/notasFiscais/RelatorioCompras.java
+++ Código/Tabacaria/src/model/notasFiscais/RelatorioCompras.java
@@ -2,8 +2,9 @@ package model.notasFiscais;
 
 public class RelatorioCompras extends Relatorio {
 
-    public RelatorioCompras(int id, String titulo, String texto) {
-        super(id, titulo, texto);
+    public RelatorioCompras(int id, String titulo, String texto, String data) {
+        super(id, titulo, texto, data);
     }
 
+    
 }
