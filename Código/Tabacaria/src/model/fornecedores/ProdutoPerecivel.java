522e42b0707dbf6510d1a336f19f40caffee3e5b
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-10-17 09:06:58 -0200

    adc class Produto Perecivel

--- Código/Tabacaria/src/model/fornecedores/ProdutoPerecivel.java
+++ Código/Tabacaria/src/model/fornecedores/ProdutoPerecivel.java
@@ -1,16 +1,19 @@
 package model.fornecedores;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 
 public class ProdutoPerecivel extends Produto {
 
-	private String dataProducao;
+	private LocalDate dataProducao;
 
-	private String dataValidade;
+	private LocalDate dataValidade;
 
 	private String modoConservacao;
 
-    public ProdutoPerecivel(String dataProducao, String dataValidade, String modoConservacao, int id, String nome, String descricao, float preco) {
+    public ProdutoPerecivel(LocalDate dataProducao, LocalDate dataValidade, String modoConservacao, int id, String nome, String descricao, float preco) {
         super(id, nome, descricao, preco);
         this.dataProducao = dataProducao;
         this.dataValidade = dataValidade;
@@ -18,19 +21,19 @@ public class ProdutoPerecivel extends Produto {
     }
 
    
-    public Date getDataProducao() {
+    public LocalDate getDataProducao() {
         return dataProducao;
     }
 
-    public void setDataProducao(Date dataProducao) {
+    public void setDataProducao(LocalDate dataProducao) {
         this.dataProducao = dataProducao;
     }
 
-    public Date getDataValidade() {
+    public LocalDate getDataValidade() {
         return dataValidade;
     }
 
-    public void setDataValidade(Date dataValidade) {
+    public void setDataValidade(LocalDate dataValidade) {
         this.dataValidade = dataValidade;
     }
 
@@ -45,7 +48,12 @@ public class ProdutoPerecivel extends Produto {
     
 
 	public boolean naValidade() {
-		return false;
+            if (Integer.parseInt(dataValidade.format(DateTimeFormatter.BASIC_ISO_DATE)) > Integer.parseInt(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE))){
+                return true;
+            } else {
+                return false;
+            }
+		
 	}
 
 }
