2b74adfb501486fe5ef1cbdd1b07b52b6125a26b
KaMalzoni <karen.malzoni@hotmail.com>
2017-10-29 16:31:45 -0200

    começando código jbehave

---
+++ Código/Tabacaria/test/jbehavetests/AdicionarProdutoSteps.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbehavetests;
+
+/**
+ *
+ * @author Karen
+ */
+
+import static org.jbehave.Ensure.ensureThat;
+import org.jbehave.scenario.annotations.Given;
+import org.jbehave.scenario.annotations.Then;
+import org.jbehave.scenario.annotations.When;
+import org.jbehave.scenario.steps.Steps;
+
+public class AdicionarProdutoSteps extends Steps {
+    
+}
